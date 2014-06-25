b5cf02742768241d52e0230a4249a2d3b96cfaa0
lenadroid <lenadroid@yandex.com>
2014-06-25 18:31:19 +0200

    added templating in html, autoinsert

--- generator.scala
+++ generator.scala
@@ -1,17 +1,19 @@
-
 import scala.xml.XML
 import scala.xml._
 import java.io._
+import scala.io.Source
+import java.io.PrintWriter
 
 object Generator {
 
-    def fw = new FileWriter("Posts.html", true)
+    val fw = new FileWriter("Posts.html", true)
+    val newLine = System.getProperty("line.separator")
 
     def isNodeEmpty(n: NodeSeq): Boolean = {
         n.text.replaceAll("\\s+$", "") == ""
     }
 
-    def isEmpty(node: Node) = {
+    def isEmpty(node: Node): Boolean = {
         node.child.filter {c => !c.isInstanceOf[Text] || !c.text.trim.isEmpty}.isEmpty
     }
 
@@ -20,13 +22,21 @@ object Generator {
         case _ => None
     }
 
-    def getStartTag(n: Node, tl: Int = 0) = {
+    def getStartTag(n: Node, tl: Int = 0):String = {
         val attrs = n.attributes.filterNot(_.key == "tag").foldLeft(List[String]())((l, a) => (a.key + "='" + a.value + "' ") :: l)
         (tab(tl) + "<" + ((n \ ("@tag")).text) + " class='"+ n.label +"' " + attrs.mkString + ">")
     }
 
-    def getEndTag(n: Node, tl: Int = 0) = {
-        (tab(tl) + "</" + ((n \ ("@tag")).text) + ">") 
+    def getEndTag(n: Node, tl: Int = 0):String = {
+        (tab(tl) + "</" + ((n \ ("@tag")).text) + ">")
+    }
+
+    def emptyTag(n: Node, tl: Int) = {
+        getStartTag(n, tl) + newLine + getEndTag(n, tl)
+    }
+
+    def tagWithContent(n:Node, tl:Int, content: String) = {
+        getStartTag(n, tl) + newLine + content + newLine + getEndTag(n, tl)
     }
 
     def getTextContent(text:String, tl: Int = 0) = {
@@ -34,46 +44,101 @@ object Generator {
     }
 
     def printTextNode(n:Node, tl: Int = 0) = {
-        print(getStartTag(n, tl), true)
         val text = textOfNode(n)
         text match {
-            case Some(s) => print(getTextContent(s, tl + 1), true)
-            case _ => print("")
+            case Some(s) => tagWithContent(n, tl, getTextContent(s, tl + 1))
+            case _ => emptyTag(n, tl)
         }
-        print(getEndTag(n, tl), true)
     }
 
     def tab(tl:Int = 0):String = {
         ("   " * tl)
     }
 
-    def displayContent(element: Node, tl:Int = 0):Unit = {
-        print(getStartTag(element, tl), true)
-        element.child.filterNot(c => isNodeEmpty(c)).map { c =>
-            textOfNode(c) match {
-                case Some(s) => printTextNode(c, tl + 1)
-                case _ => displayContent(c, tl + 1)
-            } 
-        }
-        print(getEndTag(element, tl), true)
+    def display(element:Node, tl:Int): String = {
+        element.child.filterNot(c => isNodeEmpty(c)).
+        foldLeft ("") ((acc, item) => textOfNode(item) match {
+                                        case Some(s) => acc + printTextNode(item, tl + 1) + newLine
+                                        case _ => acc + tagWithContent(item, tl + 1, display(item, tl + 2)) + newLine})
+    }
+
+    def displayContent(element: Node, tl:Int = 0):String = {
+        tagWithContent(element, tl, display(element, tl))
     }
 
     def print(s:String, toFile:Boolean = false):Unit = {
-        if(toFile) {
-            val fw = new FileWriter("Posts.html", true)
-            try {
-              fw.write(s + "\n")
-            }
-            finally fw.close() 
+        if(toFile) fw.write(s + "\n") else println(s)
+    }
+
+    def deleteFile(name: String) = {
+        try {
+            val file = new File("Posts.html")
+            if(file.delete()) println(file.getName() + " is deleted!") else println("Delete operation is failed.")
         }
-        else {
-            println(s)
+        catch {
+            case e:Exception => e.printStackTrace()
+        }
+    }
+
+    def mapFile(inFile:String, outFile:String) {
+        val pw = new PrintWriter(outFile)
+        val in = Source.fromFile(inFile)
+        for(c <- in) pw.print(c)
+        in.close
+        pw.close
+    }
+
+    def insertInside(file: String, content: String, template:String) = {
+        val tempFile = "temp.txt"
+        val pw = new PrintWriter(tempFile)
+        try { 
+            val source = Source.fromFile(file)
+            val lines = source.getLines
+            for(line <- lines) {
+                if(line.trim().equalsIgnoreCase(template)) {
+                    pw.println(line)
+                    pw.println(content)
+                }
+                else pw.println(line)
+            }
+            pw.close
+            mapFile(tempFile,file)
+            new File(tempFile).delete()
+        }
+        catch {
+            case e:Exception => e.printStackTrace()
+        }
+    }
+
+    def clearTemplateOfFile(file:String, templateStart:String, templateEnd:String) = {
+        val tempFile = "temp.txt"
+        val pw = new PrintWriter(tempFile)
+        var insideTemplate = false
+        try { 
+            val source = Source.fromFile(file)
+            val lines = source.getLines
+            for(line <- lines) {
+                if(line.trim().equalsIgnoreCase(templateStart)) {
+                    insideTemplate = true
+                    pw.println(line)
+                    pw.write("")
+                }
+                if(line.trim().equalsIgnoreCase(templateEnd)) insideTemplate = false
+                if(!insideTemplate) pw.println(line)
+            }
+            pw.close
+            mapFile(tempFile,file)
+            new File(tempFile).delete()
+        }
+        catch {
+            case e:Exception => e.printStackTrace()
         }
     }
 
     def main(args: Array[String]): Unit = {
-        new File("Posts.html").delete() 
+        clearTemplateOfFile("Posts.html", "<template>", "</template>")
         val config = XML.loadFile("posts.xml")
-        displayContent(config)
+        val result = displayContent(config, 2)
+        insertInside("Posts.html", result, "<template>")
     }
 }
