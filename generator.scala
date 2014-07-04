8be189b870895820c843674cf272a68528a8581f
lenadroid <lenadroid@yandex.com>
2014-07-04 18:02:12 +0200

    templating for html, example with yandex metrica

--- generator.scala
+++ generator.scala
@@ -25,6 +25,10 @@ object Generator {
         case _ => None
     }
 
+    def htmlComment(text: String):String = {
+        ("""<!--""" + text + """-->""")
+    }
+
     def getStartTag(n: Node, tl: Int = 0):String = {
         val attrs = n.attributes.filterNot(_.key == "tag").foldLeft(List[String]())((l, a) => (a.key + "='" + a.value + "' ") :: l)
         (tab(tl) + "<" + ((n \ ("@tag")).text) + " class='"+ n.label +"' " + attrs.mkString + ">")
@@ -83,12 +87,19 @@ object Generator {
         }
     }
 
-    def mapFile(inFile:String, outFile:String) {
+    def mapFile(inFile:String, outFile:String) = {
         val pw = new PrintWriter(outFile)
         val in = Source.fromFile(inFile)
         for(c <- in) pw.print(c)
         in.close
         pw.close
+    }
+
+    def textOfFile(file:String):String = {
+        var result = new StringBuffer()
+        val in = Source.fromFile(file)
+        for(c <- in) result.append(c)
+        return result.toString()
     }
 
     def insertInside(file: String, content: String, template:String) = {
@@ -138,6 +149,26 @@ object Generator {
         }
     }
 
+    def readConfiguration() = {
+        val config = XML.loadFile("config.xml")
+
+        val inputFile = (config \\ "inputfile").text
+        val outputFile = (config \\ "outputfile").text
+        val templateStart = (config \\ "startcommenttext").text
+        val templateEnd = (config \\ "endcommenttext").text
+        val in = if(inputFile.equals("")) Nil else inputFile
+        val out = if(outputFile.equals("")) Nil else outputFile
+        val ts = if(templateStart.equals("")) Nil else templateStart
+        val te = if(templateEnd.equals("")) Nil else templateEnd
+        println("Input file name: " + inputFile + "!")
+        println("Output file name: " + outputFile + "!")
+        println("Start template: " + templateStart + "!")
+        println("End template: " + templateEnd + "!")
+
+        (in, out, ts, te)
+        //List(inputFile, outputFile, templateStart, templateEnd).map(a => if(a.equals("")) None else Some(a))
+    }
+
     def setConfiguration(args:Array[String]) = {
         if(args.length != 0) {
             args.length match {
@@ -150,17 +181,48 @@ object Generator {
                                                    xmlInputFile = args(3))
             }
         }
+        printSettings
+    }
+
+    def printSettings = {
+        println("Input file name: " + settings.xmlInputFile)
         println("Output file name: " + settings.htmlOutputFile)
         println("Start template: " + settings.templateStart)
         println("End template: " + settings.templateEnd)
-        println("Input file name: " + settings.xmlInputFile)
     }
 
     def main(args: Array[String]): Unit = {
         setConfiguration(args)
+        // todo - foreach configuration setting do this
         clearTemplateOfFile(settings.htmlOutputFile, settings.templateStart, settings.templateEnd)
-        val config = XML.loadFile(settings.xmlInputFile)
-        val result = displayContent(config, 2)
-        insertInside(settings.htmlOutputFile, result, settings.templateStart)
+        val endshtml = settings.xmlInputFile.endsWith("html")
+        println(endshtml + " " + settings.xmlInputFile);
+        if(endshtml) {
+            insertInside(settings.htmlOutputFile,textOfFile(settings.xmlInputFile), settings.templateStart)
+        } else {
+            val posts = XML.loadFile(settings.xmlInputFile)
+            val result = displayContent(posts, 2)
+            insertInside(settings.htmlOutputFile, result, settings.templateStart)
+        }
+
+        // val sets = readConfiguration
+        // sets match {
+        //     case (in, Nil, Nil, Nil) => {
+        //         settings = settings.copy(xmlInputFile = in.toString)
+        //     }
+        //     case (Nil, out, Nil, Nil) => {
+        //         settings = settings.copy(htmlOutputFile = out.toString)
+        //     }
+        //     case (in, out, ts, Nil) => {
+        //         settings = settings.copy(xmlInputFile = in.toString, htmlOutputFile = out.toString, templateStart = ts.toString)
+        //     }
+        //     case (in, out, Nil, te) => {
+        //         settings = settings.copy(xmlInputFile = in.toString, htmlOutputFile = out.toString, templateEnd = te.toString)
+        //     }
+        //     case (in, out, ts, te) => {
+        //         settings = settings.copy(xmlInputFile = in.toString, htmlOutputFile = out.toString, templateStart = ts.toString, templateEnd = ts.toString)
+        //     }
+        // }
+        // printSettings
     }
 }
